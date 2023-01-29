31dfe5ba5f5b9ba4ff48046250a62f63abc674d1
Gaurund <pavel.makushev@gmail.com>
2023-01-29 17:22:52 +0300

    Complete task No.2

---
+++ 02/main.py
@@ -0,0 +1,39 @@
+'''
+Задача 2: Найдите сумму цифр трехзначного числа. 
+
+123 -> 6 (1 + 2 + 3)
+100 -> 1 (1 + 0 + 0)
+
+'''
+
+
+def input_integer(msg):
+    num = input(msg)
+    try:
+        return (int(num))
+    except ValueError:
+        return input_integer("Необходимо ввести именно число. Повторите ввод: ")
+
+
+def get_three_digits():
+    num = 0
+    while not (99 < num < 1000):
+        num = input_integer("Пожалуйста, введите трёхзначное число: ")
+    return num
+
+
+def summarize_digits(num):
+    total = 0
+    while num != 0:
+        total = total + num % 10
+        num = num // 10
+    return total
+
+
+def output():
+    num = get_three_digits()
+    total = summarize_digits(num)
+    print(num, "->", total)
+
+
+output()
